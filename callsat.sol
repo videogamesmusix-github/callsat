c450978edbebc9b40285a9dce5a73836cd1d6a92
videogamesmusix-github <128660259+videogamesmusix-github@users.noreply.github.com>
2023-07-22 06:06:51 -0300

    Create callsat.sol

---
+++ callsat.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+// CALLSAT by SAT v2.1 - Shows how many times this contract was called
+
+pragma solidity ^0.8.9;
+
+contract CALLSAT {
+  
+  uint callsCounter;
+
+  function callContract() public {
+      callsCounter = callsCounter + 1;
+  }
+
+  function showCounter() public view returns (uint) {
+      return callsCounter;
+  }
+
+}
