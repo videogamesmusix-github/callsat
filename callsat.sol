5b1f9d5e0e984538e003203412285a1e979e3d74
videogamesmusix <128660259+videogamesmusix-github@users.noreply.github.com>
2024-12-01 11:03:20 -0300

    Update callsat.sol

--- callsat.sol
+++ callsat.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-// CALLSAT by SAT v2.1 - Shows how many times this contract was called - 30-11-2024
+// CALLSAT by SAT v2.1 - Shows how many times this contract was called - 01-12-2024
 
 pragma solidity ^0.8.9;
 
