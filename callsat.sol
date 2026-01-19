0e2799f03f9e21ecd7ecb0a19eb3d8164acf6f22
videogamesmusix <128660259+videogamesmusix-github@users.noreply.github.com>
2026-01-19 15:10:38 -0300

    Update callsat.sol

--- callsat.sol
+++ callsat.sol
@@ -1,4 +1,4 @@
-/ CALLSAT by SAT v2.1 - Shows how many times this contract was called - 19-01-2026
+// CALLSAT by SAT v2.1 - Shows how many times this contract was called - 19-01-2026
 
 pragma solidity ^0.8.9;
 
